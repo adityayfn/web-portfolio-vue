ff961711195cc8b79856dcbbac6c5573922eebc2
adityayfn <adhitya.firstian@gmail.com>
2023-07-13 01:24:53 +0700

    update connect

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -6,17 +6,22 @@
       class="dropdown-content z-[1] menu p-2 shadow bg-slate-100 rounded-box mx-10"
     >
       <li class="my-0 mx-auto">
-        <a href="https://github.com/adityayfn"
+        <a href="https://github.com/adityayfn" class="focus:bg-green"
           ><font-awesome-icon :icon="['fab', 'github']" size="2xl"
         /></a>
       </li>
       <li class="my-0 mx-auto">
-        <a href="https://www.facebook.com/adhitya.yudha.77"
+        <a
+          href="https://www.facebook.com/adhitya.yudha.77"
+          class="focus:bg-green"
           ><font-awesome-icon :icon="['fab', 'facebook']" size="2xl"
         /></a>
       </li>
       <li class="my-0 mx-auto">
-        <a href="https://www.linkedin.com/in/aditya-yudha-firstian/">
+        <a
+          href="https://www.linkedin.com/in/aditya-yudha-firstian/"
+          class="focus:bg-green"
+        >
           <font-awesome-icon :icon="['fab', 'linkedin']" size="2xl"
         /></a>
       </li>
